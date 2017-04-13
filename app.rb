a9cae526b4a0da33475739a0a33cc5eecaab4a09
inzoddwetrust <inzoddwetrust@gmail.com>
2017-04-14 02:54:55 +0600

    DB added, init_db

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new './public/main.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -8,4 +18,9 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+	@new_post = params[:new_post]
+	erb "#{@new_post.gsub("\n", "<br>")}"
 end
