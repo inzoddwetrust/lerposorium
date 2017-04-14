3ab7b5635869a62fd963e8ce0727c2e824a07ab4
inzoddwetrust <inzoddwetrust@gmail.com>
2017-04-15 03:44:34 +0600

    Configure Do

--- app.rb
+++ app.rb
@@ -8,6 +8,16 @@ def init_db
 	@db.results_as_hash = true
 end
 
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
+							(
+								`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+								`created_date` DATE,
+								`content` TEXT
+							)'
+end
+
 before do
 	init_db
 end
