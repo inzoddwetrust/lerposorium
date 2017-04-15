337b4a8c745736db027e441dce07dccbbe1e2efc
inzoddwetrust <inzoddwetrust@gmail.com>
2017-04-15 09:44:00 +0600

    New comments are storing to database

--- app.rb
+++ app.rb
@@ -8,12 +8,25 @@ def init_db
 	@db.results_as_hash = true
 end
 
+def prepare_enrty
+	@post_id=params[:post_id]
+	@content = @db.execute 'SELECT * FROM Posts where id=?', [@post_id]
+	@post_comments = @db.execute 'SELECT * FROM Comments where post_id=?', [@post_id]
+end
+
 configure do
 	init_db
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
 							(
 								`id` INTEGER PRIMARY KEY AUTOINCREMENT,
 								`created_date` DATE,
+								`content` TEXT
+							)'
+	@db.execute 'CREATE TABLE IF NOT EXISTS Comments
+							(
+								`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+								`created_date` DATE,
+								`post_id` INTEGER,
 								`content` TEXT
 							)'
 end
@@ -32,25 +45,24 @@ get '/new' do
 end
 
 get '/entry/:post_id' do
-	@post_id=params[:post_id]
-	@content = @db.execute 'SELECT * FROM Posts where id=?', [@post_id]
-
+	prepare_enrty
 	erb :entry
 end
 
 post '/entry/:post_id' do
 	@post_id=params[:post_id]
-	@content = @db.execute 'SELECT * FROM Posts where id=?', [@post_id]
-
 	new_comment = params[:new_comment].gsub("\n", "<br>")
 
 	if new_comment.empty?
 		@error = "Enter your comment"
+		prepare_enrty
 		return erb :entry
 	end
 
-	erb :entry
-	# erb "#{params[:new_comment]}"
+	@db.execute 'INSERT INTO Comments (created_date, post_id, content) VALUES (datetime(), ?, ?)', [@post_id, new_comment]
+
+	prepare_enrty
+	redirect to('/entry/'+ @post_id)
 end
 
 post '/new' do
@@ -61,6 +73,7 @@ post '/new' do
 		return erb :new
 	end
 
-	@db.execute 'INSERT INTO Posts (created_date, content) VALUES (datetime(), ?)', [new_post]
+	@db.execute "INSERT INTO Posts (created_date, content) VALUES (datetime(), '#{new_post}')"
+
 	redirect to '/'
 end
